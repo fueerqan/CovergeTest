b43047de892c8536ce7a8a86bd1eaadee1ba29ee
wni <wni@wni-no-MacBook-Air.local>
2017-02-01 16:39:05 +0900

    second commit

---
+++ autoCoverageTest.pl
@@ -0,0 +1,23 @@
+use strict;
+use warnings;
+
+# delete previous Coverage Test Data if exists
+system "cover -delete";
+
+# run perl BuildTest.pl
+system "perl BuildTest.PL";
+
+if( $? == -1 ){
+	print "Failed to run Coverage Test : $!\n";
+}else{
+	print "Coverage Test Successed : \n";
+}
+
+# convert Coverage Summary to HTML format
+print "Converting Coverage test to HTML format\n";
+
+system "cover";
+
+if($? == -1){
+	print "Failed to convert to HTML : $!\n";
+}
